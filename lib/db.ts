fe2eadd4e509a26f6e5ecab130202c44536cc522
tgrconsulting <tgrconsulting@hotmail.com>
2026-01-30 18:34:49 +0100

    Add SSL db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,10 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Supabase/Vercel connectivity
+    }
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
+
