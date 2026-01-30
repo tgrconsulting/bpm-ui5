9137098ba7c6910a37bfe371b140e0ea1677f1b6
tgrconsulting <tgrconsulting@hotmail.com>
2026-01-30 18:09:59 +0100

    Add Vercel debugging information

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    logging: {
+        fetches: {
+            fullUrl: true,
+        },
+    },
+};
+
+console.log("--- VERCEL BUILD CHECK ---");
+console.log("AUTH_GOOGLE_ID exists:", !!process.env.AUTH_GOOGLE_ID);
+console.log("AUTH_GOOGLE_SECRET exists:", !!process.env.AUTH_GOOGLE_SECRET);
+console.log("--------------------------");
 
 module.exports = nextConfig;
