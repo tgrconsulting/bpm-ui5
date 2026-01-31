10b0a161c486be72fcac78fd4e7e3d681556a703
tgrconsulting <tgrconsulting@hotmail.com>
2026-01-31 13:22:25 +0100

    Fix import error

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 // auth.ts
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import { query } from "@/lib/db"
+//import { query } from "@/lib/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
