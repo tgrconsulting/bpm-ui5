e0e408f230a397946d3f12b54a66efde515b99f3
tgrconsulting <tgrconsulting@hotmail.com>
2026-02-07 16:52:58 +0100

    DB add process_status

--- supabase/migrations/20260207143253_add_process_status.sql
+++ supabase/migrations/20260207143253_add_process_status.sql
@@ -1,5 +1,3 @@
 ALTER TABLE tbl_processes 
 ADD COLUMN process_status varchar(1) 
-CHECK (process_status IN ('A', 'I'));ALTER TABLE tbl_processes 
-ADD COLUMN process_status varchar(1) 
 CHECK (process_status IN ('A', 'I'));
