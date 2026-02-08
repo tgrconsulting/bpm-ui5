dba628e30ad1e384c74095c5e758d2722c85065e
tgrconsulting <tgrconsulting@hotmail.com>
2026-02-08 18:00:38 +0100

    duration set null

--- supabase/migrations/20260208161739_add_duration.sql
+++ supabase/migrations/20260208161739_add_duration.sql
@@ -3,4 +3,3 @@ ADD COLUMN duration integer;
 
 ALTER TABLE "public"."tbl_processevents" ALTER COLUMN "application_id" SET NOT NULL;
 ALTER TABLE "public"."tbl_processevents" ALTER COLUMN "description" SET NOT NULL;
-ALTER TABLE "public"."tbl_processevents" ALTER COLUMN "duration" SET NOT NULL;
